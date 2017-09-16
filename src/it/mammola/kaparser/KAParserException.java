07eb6d8821f0458d0788419ac6a7d9a0232da533
Domenico Mammola <mimmo71@gmail.com>
2017-09-16 14:50:55 +0200

    * wrong comments

--- src/it/mammola/kaparser/KAParserException.java
+++ src/it/mammola/kaparser/KAParserException.java
@@ -1,14 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  This is part of the KaParser4Java Library
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This software is distributed without any warranty.
+ *
+ * @author Domenico Mammola (mimmo71@gmail.com - www.mammola.net)
+ *
  */
 package it.mammola.kaparser;
 
-/**
- *
- * @author E5430
- */
 public class KAParserException extends Exception {
 
     private static final long serialVersionUID = 7436734623842384762L;
